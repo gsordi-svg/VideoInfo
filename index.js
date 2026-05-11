086936283ff08aa8a612ad2fa27d5bdb5c065748
Gabriella Sordi <gabriellasordi@Gabriellas-MacBook-Air.local>
2026-05-10 23:12:42 -0400

    fixed some errors

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const { app, BrowserWindow } = electron;
 
 app.on('ready', function() {
     console.log("App is ready");
-    const mainWindow = new BrowserWindow({});
+    const mainWindow = new BrowserWindow({
+        webPrefrences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
